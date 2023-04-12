66958d13f9b11cab1866a651ea768061a50cf404
mayank18garg <mayank.ind18@gmail.com>
2023-04-12 12:09:02 -0700

    usage analytics 2

--- Supercharging-portal-frontend/src/index.js
+++ Supercharging-portal-frontend/src/index.js
@@ -6,7 +6,9 @@ import { App } from "./app";
 import "rsuite/dist/rsuite.min.css";
 import "./styles/styles.css";
 import { Auth0ProviderWithNavigate } from "./auth0-provider-with-navigate";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-BEF4WKS30Z");
 const container = document.getElementById("root");
 const root = createRoot(container);
 
